83c86ac48b141038eb7e71f453c8c80b0d82e0f0
Akshay <akshay.kumar3851@gmail.com>
2020-04-07 10:23:09 +0530

    initial commit
    initial commit added and removed default files

---
+++ src/ProductCategoryRow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class ProductCategoryRow extends React.Component {
+    render() {
+        const category = this.props.category;
+        return (
+            <tr>
+                <th colSpan="2">
+                    {category}
+                </th>
+            </tr>
+        );
+    }
+}
+
+export default ProductCategoryRow
